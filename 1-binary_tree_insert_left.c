b83693bd4c6e2355a8b65a7d1430c8276e25aec1
majdi aribi <majdi.developer@gmail.com>
2022-09-01 15:19:25 +0100

    :white_check_mark:seecond task :white_check_mark:

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,39 @@
+
+
+
+
+
+#include "binary_trees.h"
+/**
+ * binary_tree_node - function create new node with parent
+ * @parent: Pointer to the parent node
+ * @value: Integer stored in the node
+ * Return: Pointer too the new node
+ * else return NULL
+ *
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *tempNode =
+		(binary_tree_t *) malloc(sizeof(binary_tree_t));
+
+	if (!tempNode)
+		return (NULL);
+
+	tempNode->parent = parent;
+	tempNode->n = value;
+	tempNode->right = NULL;
+	tempNode->left = NULL;
+
+
+	if (parent->left != NULL)
+	{
+		tempNode->left = parent->left;
+		parent->left->parent = tempNode	;
+	}
+	parent->left = tempNode;
+
+
+
+	return (tempNode);
+}
