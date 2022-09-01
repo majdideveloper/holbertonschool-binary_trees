70d3524d847301b102d80a583a3089809a03547d
majdi aribi <majdi.developer@gmail.com>
2022-09-01 11:20:59 +0100

    :tada: first task  :tada:

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -15,7 +15,7 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	 binary_tree_t *tempNode =
-		( binary_tree_t *) malloc(sizeof( binary_tree_t));
+		(binary_tree_t *) malloc(sizeof(binary_tree_t));
 
 	if (!tempNode)
 		return (NULL);
