74bb9901314fcf54fb4fe20e2a6fc4794f3157a8
majdi aribi <majdi.developer@gmail.com>
2022-09-01 17:15:06 +0100

    :fire:print all value of tree inorder :fire:

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,24 @@
+
+
+
+
+
+#include "binary_trees.h"
+/**
+ * binary_tree_inorder - function print all node in tree with inorder
+ * @tree: Pointer to the root of tree
+ * @func: Pointer to the function
+ * Return: Nothing ...
+ *
+ */
+	void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && func)
+	{
+		binary_tree_inorder(tree->left, func);
+		func(tree->n);
+		binary_tree_inorder(tree->right, func);
+	}
+
+
+}
