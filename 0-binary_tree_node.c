9b6ff49ce1f045ef6232f6548a3ac4559b2c75fb
majdi aribi <majdi.developer@gmail.com>
2022-09-01 11:16:39 +0100

    :tada: first task  :tada:

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -5,27 +5,25 @@
 
 #include "binary_trees.h"
 /**
- * binary_tree_node - function create new node with parent 
- * @parent : Pointer to the parent node
- * @value : Integer stored in the node
- * Return Pointer too the new node 
- * else return NULL 
+ * binary_tree_node - function create new node with parent
+ * @parent: Pointer to the parent node
+ * @value: Integer stored in the node
+ * Return: Pointer too the new node
+ * else return NULL
  *
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	struct  binary_tree_s *tempNode = 
+	struct  binary_tree_s *tempNode =
 		(struct binary_tree_s *) malloc(sizeof(struct binary_tree_s));
 
-	if(!tempNode)
+	if (!tempNode)
 		return (NULL);
-	
+
 	tempNode->parent = parent;
 	tempNode->n = value;
 	tempNode->left = NULL;
 	tempNode->right = NULL;
 
-
-	
 	return (tempNode);
 }
