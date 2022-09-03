bb7dc9f8454fb6bbead7271affeb417d9b5cddf0
majdi aribi <majdi.developer@gmail.com>
2022-09-03 07:03:51 +0100

    :recycle: function depth :recycle:

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -11,10 +11,12 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	while (tree->parent)
-	{
-		depth += 1;
-		tree = tree->parent;
-	}
+depth = tree->parent ? 1 + binary_tree_depth(tree->parent) : 0;	
+/**	while (tree->parent)
+*	{
+*		depth += 1;
+*		tree = tree->parent;
+*	}
+*/
 	return (depth);
 }
