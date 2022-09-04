7246c04936ea8920f59771401265955c028ccd70
AbdelmaoulaAbdou <talbiabdelmaoula@gmail.com>
2022-09-04 17:22:59 +0100

    fix betty style

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -36,5 +36,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	if (!tree->left && !tree->right)
 		return (1);
-	return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
+	return (binary_tree_is_perfect(tree->left)
+			&& binary_tree_is_perfect(tree->right));
 }
