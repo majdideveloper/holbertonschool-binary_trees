508c0dd9d1c3a59858fdcd6585c5dc7cf981c56b
majdi aribi <majdi.developer@gmail.com>
2022-09-04 08:02:09 +0100

    :monocle_face: uncle and sibling :monocle_face:

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,16 +1,17 @@
 #include"binary_trees.h"
 /**
- * binary_tree_is_leaf - function delete all tree of node
- * @node: Pointer to the node  to cheak is leaf or not
- * Return: Int 1 if leaf
+ * binary_tree_sibling - function return if sibling 
+ * @node: Pointer to the node  to cheak is sibling or not
+ * Return: Int 1 if sibling
  * else 0
  *
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
+	binary_tree_t *temp = node;
+
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
-	binary_tree_t *temp = node;
 	node = node->parent;
 	if (node->left == temp)
 		node = node->right;
@@ -19,5 +20,5 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	else if ((node->right == temp && node->left == NULL)
 			|| (node->left == temp && node->right == NULL))
 		return (NULL);
-return (node);
+	return (node);
 }
